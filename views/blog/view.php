b7e69dbc27fe6830aeb9bfbc90fd1582ca1a19ab
potap <a-potap@mail.ru>
2017-12-09 22:37:28 +0400

    some fix

--- views/blog/view.php
+++ views/blog/view.php
@@ -1,17 +1,30 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\DetailView;
+
+$this->title = $model->title;
 
 ?>
 <div class="blog-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <div>
+        <?= $model->date;?>
+    </div>
+
     <?= $model->text;?>
 
-    <div>
-
+    <div class="comments">
+        <?= \yii\widgets\ListView::widget([
+            'dataProvider' => new ActiveDataProvider([
+                'query' => $model->comments,
+            ]),
+            'itemView' => '_comment',
+            'viewParams' => [
+                'fullView' => false,
+            ],
+        ]);?>
     </div>
 
 </div>
