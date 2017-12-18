f5457e55e5a2ae4a11d87542ec4f47fef3f0c3a3
potap <potap@russianit.ru>
2017-12-18 21:29:29 +0400

    format comments and musik page

--- views/blog/_comment.php
+++ views/blog/_comment.php
@@ -2,10 +2,12 @@
 
 ?>
 
-<div class="comment">
-    <span><?=$model->iduser?></span>
-    <span><?=$model->date?></span>
-    <p>
-        <?=$model->text?>
-    </p>
+<div class=" row comment">
+    <div class="col-xs-11 col-xs-offset-1">
+        <strong><?=$model->iduser?></strong>
+        <span><?=$model->date?></span>
+        <p>
+            <?=$model->text?>
+        </p>
+    </div>
 </div>
--- views/blog/view.php
+++ views/blog/view.php
@@ -15,12 +15,19 @@ $this->title = $model->title;
 
     <?= $model->text;?>
 
+
+
     <div class="comments">
+        <p>
+            <strong>Комментарии:</strong>
+        </p>
+
         <?= \yii\widgets\ListView::widget([
             'dataProvider' => new \yii\data\ActiveDataProvider([
                 'query' => \app\models\BlogComents::find()->where(['idpost' => $model->id]),
             ]),
             'itemView' => '_comment',
+            'layout' =>  "{items} {pager}",
             'emptyText' => 'Ещё нет коментариев'
         ]);?>
     </div>
