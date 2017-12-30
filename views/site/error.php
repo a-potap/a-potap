bbd6eb9b576c5f98b3fc1dd10d4830bb3d25275b
potap <a-potap@mail.ru>
2017-12-30 19:44:27 +0400

    error page

--- views/site/error.php
+++ views/site/error.php
@@ -16,7 +16,12 @@ $this->title = 'Упс, ошибка(';
     <div class="alert alert-danger">
         <p><b><?= nl2br(Html::encode($message)) ?></b></p>
         <p>
-            Как всегда что то пошло не так. Но ты не огорчайся, все будет отлично, просто вернись на главную.
+            Как всегда что то пошло не так. Но ты не огорчайся, просто вернись на <a href="/"
+                                                                                     style="color: inherit; text-decoration: underline">главную</a>.
+        </p>
+        <p>
+            Но если это что то важное, напиши, пожалуйста, мне на почту <a href="mailto:a-potap@mail.ru"
+                                                                           style="font-weight: bold; color: inherit; text-decoration: underline">a-potap@mail.ru</a>
         </p>
     </div>
 </div>
