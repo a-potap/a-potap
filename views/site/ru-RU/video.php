de268b14158baf9d40966f37767bf392a52c2d4c
potap <potap@russianit.ru>
2019-09-24 21:15:53 +0400

    add videos

--- views/site/ru-RU/video.php
+++ views/site/ru-RU/video.php
@@ -1,5 +1,5 @@
 <?php
-$this->title = 'Video';
+$this->title = 'Видео';
 
 ?>
 
@@ -8,12 +8,13 @@ $this->title = 'Video';
 
     <div class="row">
         <div class="col-xs-12">
-            <h2>Helsinki 2019</h2>
+            <h2>Хельсинки 2019</h2>
             <p>2019-07-29</p>
             <p>
-                24 hours in Hensilki. It was cool. Beautiful buildings, an amusement park,
-                the metro where the clip was shot for old song Bomfunk MC's - Freestyler and the harsh nightlife.
-                Only I didn’t have enough phone charge to shot what is happening at night, probably it’s even better.
+                24 часа в хенсильки. Это было круто. Красивые здания, парк развлечений,
+                метро где сняли клип на тарую песню Bomfunk MC's - Freestyler и суровая
+                ночная жизнь. Только у меня не хватило заряда телефона что бы снять что творится ночью,
+                наверное это даже лучше.
             </p>
             <div class="embed-responsive embed-responsive-16by9">
                 <video class="embed-responsive-item" controls>
@@ -25,12 +26,12 @@ $this->title = 'Video';
 
     <div class="row">
         <div class="col-xs-12">
-            <h2>Sochi 2018</h2>
+            <h2>Сочи 2018</h2>
             <p>2018-08-29</p>
             <p>
-                I love Sochi. Mountains, sea, sunsets. Rosa Khutor is one of the most beautiful places on earth.
-                Almost every year I go there. This time we went with my sister on a mini-trip,
-                in honor of her admission to the university.
+                Люблю Сочи. Горы, море, закаты. Роза Хутор - одно из самых красивых мест на земле.
+                Почти каждый год бываю там. Вэтот раз ездили с сестрой в минипутешествие,
+                в честь её поступления в университет.
             </p>
             <div class="embed-responsive embed-responsive-16by9">
                 <video class="embed-responsive-item" controls>
@@ -42,10 +43,10 @@ $this->title = 'Video';
 
     <div class="row">
         <div class="col-xs-12">
-            <h2>Izhevsk 2016</h2>
+            <h2>Ижевск 2016</h2>
             <p>2016-09-05</p>
             <p>
-                Morning mists on the outskirts of the city.
+                Утренние туманы у нас за домом, на окраине города.
             </p>
             <div class="embed-responsive embed-responsive-16by9">
                 <video class="embed-responsive-item" controls>
@@ -57,11 +58,11 @@ $this->title = 'Video';
 
     <div class="row">
         <div class="col-xs-12">
-            <h2>B. Sardyk 2016</h2>
+            <h2>Б. Сардык 2016</h2>
             <p>2016-07-18</p>
             <p>
-                Friends gave a quadrocopter, tried a new video format. There is nothing better than spending a
-                summer weekend in the village. Barbecue, fresh air, bath. Here I grew up.
+                Друзья подарили квадрокоптер, попробовал новый формат видеосъемки. Нет ни чего лучше,
+                чем проводить летние выходные в деревне. Шашлык, свежый воздух, баня. Здесь я вырос.
             </p>
             <div class="embed-responsive embed-responsive-16by9">
                 <video class="embed-responsive-item" controls>
@@ -73,11 +74,11 @@ $this->title = 'Video';
 
     <div class="row">
         <div class="col-xs-12">
-            <h2>Sochi 2008</h2>
+            <h2>Сочи 2008</h2>
             <p>2009-11-25</p>
             <p>
-                My first trip with friends. Heat, sea and an unknown world ahead.
-                The first time I saw the sea and mountains. That is how my love for travel came about.
+                Первое моя поездка с друзьями. Жара, море и неизведанный мир впереди.
+                Так появилась моя любовь к путешествиям.
             </p>
             <div class="embed-responsive embed-responsive-16by9">
                 <video class="embed-responsive-item" controls>
